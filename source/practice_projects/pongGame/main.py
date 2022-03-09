cd31a8b17a372a714a70b7c65ae7ba133513ead1
Anouk Driessen <info@anoukdriessen.nl>
2022-03-09 12:17:39 +0100

    Create main.py
    setup requirements

---
+++ source/practice_projects/pongGame/main.py
@@ -0,0 +1,19 @@
+# create the screen
+
+# create and move a paddle
+
+# create a second paddle
+
+# create the ball
+
+# make the ball move constantly
+
+# detect collicion with the wall -> bounce
+
+# detect collicion with paddle
+
+# detect when paddle misses
+
+# keep track of score
+
+# winning condition
